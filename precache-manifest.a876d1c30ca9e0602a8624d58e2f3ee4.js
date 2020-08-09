b3f15871076bc2072c28b57962886b4c195dd82f
Suryakumar Arumugam <asdsurya@gmail.com>
2020-08-09 21:31:18 +0530

    Updates

---
+++ precache-manifest.a876d1c30ca9e0602a8624d58e2f3ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5485ced8a9354d5db344d250cfef0f57",
+    "url": "/hacker-polls/index.html"
+  },
+  {
+    "revision": "575ccd28d9051c2e25df",
+    "url": "/hacker-polls/static/css/main.a3acf6ab.chunk.css"
+  },
+  {
+    "revision": "cecfe12081917571d51e",
+    "url": "/hacker-polls/static/js/2.2f534f72.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/hacker-polls/static/js/2.2f534f72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575ccd28d9051c2e25df",
+    "url": "/hacker-polls/static/js/main.c969853f.chunk.js"
+  },
+  {
+    "revision": "e4c4504e5e51352e3dc9",
+    "url": "/hacker-polls/static/js/runtime-main.69e25f22.js"
+  }
+]);
